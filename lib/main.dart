68244918ac9f4902b4dcca6a87d15af08956b394
Eshaan-B <eb7127@srmist.edu.in>
2020-12-30 16:59:48 +0530

    folder structure redefined

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
-
+import 'screens/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Map<int, Color> color = {
+      50: Color.fromRGBO(136, 14, 79, .1),
+      100: Color.fromRGBO(136, 14, 79, .2),
+      200: Color.fromRGBO(136, 14, 79, .3),
+      300: Color.fromRGBO(136, 14, 79, .4),
+      400: Color.fromRGBO(136, 14, 79, .5),
+      500: Color.fromRGBO(136, 14, 79, .6),
+      600: Color.fromRGBO(136, 14, 79, .7),
+      700: Color.fromRGBO(136, 14, 79, .8),
+      800: Color.fromRGBO(136, 14, 79, .9),
+      900: Color.fromRGBO(136, 14, 79, 1),
+    };
     return MaterialApp(
-      title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xffb088f9, color),
+        accentColor: MaterialColor(0xffda9ff9, color),
+        canvasColor: Color(0xfffcf8ec),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            headline6: TextStyle(
+              fontSize: 20,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.bold,
+            )),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      title: 'Fapp',
+      routes: {
+        '/':(ctx)=>CategoriesScreen(),
+        CategoryMealsScreen.routeName :(ctx)=>CategoryMealsScreen()
+      },
     );
   }
 }
