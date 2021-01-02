72c8bcb97a0da16976f08848e30853d408f7a99c
Eshaan-B <eb7127@srmist.edu.in>
2021-01-02 21:14:57 +0530

    Added filter switches

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -9,16 +9,57 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
-  bool _glutenFree=false;
+  bool _glutenFree = false;
   bool _vegetarian = false;
   bool _vegan = false;
   bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+      String title, String subtitle, bool value, Function updateValue) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Your Filters')),
       drawer: MainDrawer(),
-
+      body: Column(
+        children: [
+          _buildSwitchListTile(
+              'Gluten Free', 'Show only gluten-free meals', _glutenFree,
+              (newValue) {
+            setState(() {
+              _glutenFree = newValue;
+            });
+          }),
+          _buildSwitchListTile(
+              'Vegetarian', 'Show only vegetarian meals', _vegetarian,
+              (newValue) {
+            setState(() {
+              _vegetarian = newValue;
+            });
+          }),
+          _buildSwitchListTile('Vegan', 'Show only vegan meals', _vegan,
+              (newValue) {
+            setState(() {
+              _vegan = newValue;
+            });
+          }),
+          _buildSwitchListTile(
+              'Lactose Free', 'Show only lactose-free meals', _lactoseFree,
+              (newValue) {
+            setState(() {
+              _lactoseFree = newValue;
+            });
+          }),
+        ],
+      ),
     );
   }
 }
